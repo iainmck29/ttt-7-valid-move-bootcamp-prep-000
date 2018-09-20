3fc42bcb63cbe11d2f0fa256c001a64ba2b6182e
Iain McKenzie <iainmck29@hotmail.com>
2018-09-20 21:16:16 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-	spot = position - 1
-	if !position_taken?(board, spot) && spot.between?(0,8)
+	
+	if !position_taken?(board, position) && spot.between?(0,8)
 		true
  	else
 		false
